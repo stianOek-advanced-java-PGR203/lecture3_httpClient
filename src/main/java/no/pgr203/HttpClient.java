7745c3f00afc4ba0908f608a57cd8489e72d9e0b
StianOek <66445281+StianOek@users.noreply.github.com>
2021-09-07 09:29:15 +0200

    Test junit added to project ( working )

--- src/main/java/no/pgr203/HttpClient.java
+++ src/main/java/no/pgr203/HttpClient.java
@@ -6,20 +6,26 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class HttpClient {
-                //request >
+    public HttpClient(String host, int port, String headerfield) {
+
+    }
+    //request >
      //User-Agent UA---------------------------------O Origin
-    //                              < Response
+              //                              < Response
 
     public static void main(String[] args) throws IOException {
-
-        Socket socket = new Socket("stianhermansen.com", 80);
-
-        String request = "GET /html HTTP/1.1\r\n" + "Host: stianhermansen.com\r\n" + "\r\n";
+        // Socket er tilkoblingen mellom client og server
+        Socket socket = new Socket("urlecho.appspot.com", 80);
+        // Setter en String til å holde på en GET request til serveren.
+        // Strengen innholder to header fields, med carriage return og line feed på slutten av hver linje,
+        // slik at vi sender inn noe HTTP protokollen forstår.
+        String request = "GET /html HTTP/1.1\r\n" + "Host: urlecho.appspot.com\r\n" + "\r\n";
         socket.getOutputStream().write(request.getBytes());
 
 
         InputStream in = socket.getInputStream();
         int c;
+
         while((c = in.read()) != -1) {
             System.out.print((char)c);
         }
---
+++ src/test/java/no/pgr203/HttpClientTest.java
@@ -0,0 +1,15 @@
+package no.pgr203;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpClientTest {
+
+    @Test
+    void dummyTest() {
+        assertEquals(42, 6*7);
+    }
+
+
+}
